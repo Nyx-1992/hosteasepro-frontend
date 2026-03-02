676dabbdebd13ea79cd48444fad4b3fbbd5a8ddb
Nicole Sandra Babczyk <NicoleWork@Nicoles-MacBook-Air-2.local>
2026-03-02 20:10:18 +0200

    chore: trigger Vercel redeploy with trivial comment

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,6 +28,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Import services
+// Trigger Vercel redeploy: trivial comment
 const ICalService = require('./services/ICalService');
 
 // Connect to MongoDB
